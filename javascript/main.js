935dbe4946908d5a6d4410b99a213d0aad0bccfe
TXT-46 <txt-46@MacBook-Pro.local>
2020-07-28 21:30:06 -0700

    First commit

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+function changeTextColor(){
+    var text_color = document.getElementById('textChange');
+    text_color.style.color = "red";
+}
+function changeTextColor2(){
+    var text_color2 = document.getElementById('textChange');
+    text_color2.style.color = "blue";
+}
